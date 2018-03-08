21255a747449e167e0e06083bba39230aabade4f
Johelvis Guzman <johelvisguzman@gmail.com>
2018-03-08 12:58:16 -0500

    Introduced a repository factory

---
+++ src/DotNetToolkit.Repository.EntityFramework/EfRepositoryFactory.cs
@@ -0,0 +1,57 @@
+﻿namespace DotNetToolkit.Repository.EntityFramework
+{
+    using System;
+    using System.Data.Entity;
+
+    /// <summary>
+    /// An implementation of <see cref="IRepositoryFactory" />.
+    /// </summary>
+    public class EfRepositoryFactory : IRepositoryFactory
+    {
+        #region Private Methods
+
+        private static DbContext GetDbContext(IRepositoryOptions options)
+        {
+            if (options.DbContextType == null)
+                throw new InvalidOperationException($"The repository options must provide a {nameof(options.DbContextType)}.");
+
+            DbContext context;
+
+            if (string.IsNullOrEmpty(options.ConnectionString))
+                context = (DbContext)Activator.CreateInstance(options.DbContextType);
+            else
+                context = (DbContext)Activator.CreateInstance(options.DbContextType, options.ConnectionString);
+
+            return context;
+        }
+
+        #endregion
+
+        #region Implementation of IRepositoryFactory
+
+        /// <summary>
+        /// Creates a new repository for the specified entity type.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="options">The options.</param>
+        /// <returns>The new repository.</returns>
+        public IRepository<TEntity> Create<TEntity>(IRepositoryOptions options) where TEntity : class
+        {
+            return new EfRepository<TEntity>(GetDbContext(options));
+        }
+
+        /// <summary>
+        /// Creates a new repository for the specified entity and primary key type.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <typeparam name="TKey">The type of the key primary key value.</typeparam>
+        /// <param name="options">The options.</param>
+        /// <returns>The new repository.</returns>
+        public IRepository<TEntity, TKey> Create<TEntity, TKey>(IRepositoryOptions options) where TEntity : class
+        {
+            return new EfRepository<TEntity, TKey>(GetDbContext(options));
+        }
+
+        #endregion
+    }
+}
---
+++ src/DotNetToolkit.Repository/IRepositoryOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNetToolkit.Repository
+{
+    using System;
+
+    /// <summary>
+    /// Represents a repository option for creating new repositories.
+    /// </summary>
+    public interface IRepositoryOptions
+    {
+        /// <summary>
+        /// Gets or sets the connection string.
+        /// </summary>
+        string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the database context.
+        /// </summary>
+        Type DbContextType { get; set; }
+    }
+}
