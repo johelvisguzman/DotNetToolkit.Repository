0b6bd0eb7d2884e9d95db263c7950f57e224c151
Johelvis Guzman <johelvisguzman@gmail.com>
2018-03-17 04:35:29 -0400

    Remove the JSON.NET TypeNameHandling from the serializer

--- src/DotNetToolkit.Repository.Json/JsonRepositoryBase.cs
+++ src/DotNetToolkit.Repository.Json/JsonRepositoryBase.cs
@@ -49,8 +49,7 @@
             var serializer = new JsonSerializer
             {
                 Formatting = Formatting.Indented,
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                TypeNameHandling = TypeNameHandling.All
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
             };
 
             serializer.Serialize(writer, entities);
