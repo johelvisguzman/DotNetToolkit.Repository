5a5474afa1ab706b8396b661275d80b84c16b606
Johelvis Guzman <johelvisguzman@gmail.com>
2018-06-21 07:15:16 -0400

    Changed the way we generate a random file for the database

--- test/DotNetToolkit.Repository.Integration.Test/Data/TestEfDbContextFactory.cs
+++ test/DotNetToolkit.Repository.Integration.Test/Data/TestEfDbContextFactory.cs
@@ -1,13 +1,12 @@
 ﻿namespace DotNetToolkit.Repository.Integration.Test.Data
 {
     using System.Data.Common;
-    using System.IO;
 
     public static class TestEfDbContextFactory
     {
         public static TestEfDbContext Create()
         {
-            var currentFile = Path.GetTempFileName();
+            var currentFile = TestPathHelper.GetTempFileName();
             var connectionString = $"Data Source={currentFile};Persist Security Info=False";
             var conn = DbProviderFactories.GetFactory("System.Data.SqlServerCe.4.0").CreateConnection();
 
