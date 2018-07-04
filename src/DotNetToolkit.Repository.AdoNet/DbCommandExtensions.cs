bcaadb9b58ff68f06d2837ff554acb8ec9a98bc8
Johelvis Guzman <johelvisguzman@gmail.com>
2018-07-04 14:52:57 -0400

    Renamed the DbExtensions to DbCommandExtensions and moved it's location to the root folder

--- src/DotNetToolkit.Repository.AdoNet/DbCommandExtensions.cs
+++ src/DotNetToolkit.Repository.AdoNet/DbCommandExtensions.cs
@@ -1,11 +1,20 @@
-﻿namespace DotNetToolkit.Repository.AdoNet.Internal
+﻿namespace DotNetToolkit.Repository.AdoNet
 {
     using System;
     using System.Collections.Generic;
     using System.Data.Common;
 
-    internal static class DbExtensions
+    /// <summary>
+    /// Contains various extensions for <see cref="DbCommand" />.
+    /// </summary>
+    public static class DbCommandExtensions
     {
+        /// <summary>
+        /// Adds a new parameter with the specified name and value to the command.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="value">The value.</param>
         public static void AddParmeter(this DbCommand command, string name, object value)
         {
             if (command == null)
@@ -19,6 +28,12 @@
             command.Parameters.Add(parameter);
         }
 
+        /// <summary>
+        /// Adds a new collection of parameters to the command.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <exception cref="System.ArgumentNullException">command</exception>
         public static void AddParmeters(this DbCommand command, Dictionary<string, object> parameters)
         {
             if (command == null)
