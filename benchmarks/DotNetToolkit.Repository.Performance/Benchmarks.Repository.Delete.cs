323be4caea7c32bc9fdeb050e57195ec7b0ec9d0
Johelvis Guzman <johelvisguzman@gmail.com>
2018-11-07 07:46:20 -0500

    Added a benchmark project

---
+++ benchmarks/DotNetToolkit.Repository.Performance/Benchmarks.Repository.Delete.cs
@@ -0,0 +1,78 @@
+﻿namespace DotNetToolkit.Repository.Performance
+{
+    using BenchmarkDotNet.Attributes;
+    using Data;
+    using System.Threading.Tasks;
+
+    [BenchmarkCategory("RepositoryTraits")]
+    public class Repository_Delete_Benchmarks : BenchmarkBase
+    {
+        private IRepository<Customer> _repo;
+        private Customer _customer;
+
+        [IterationSetup]
+        public void Setup()
+        {
+            BaseSetup();
+
+            _customer = new Customer { Name = "Random Name" };
+
+            _repo = new Repository<Customer>(BuildOptions(Provider));
+            _repo.Add(_customer);
+        }
+
+        [IterationCleanup]
+        public void Clean()
+        {
+            _repo.Delete(x => x.Id != 0);
+        }
+
+        [BenchmarkCategory("Delete"), Benchmark]
+        public void Delete()
+        {
+            _repo.Delete(_customer);
+        }
+
+        [BenchmarkCategory("Delete"), Benchmark]
+        public void DeleteRange()
+        {
+            _repo.Delete(new[] { _customer });
+        }
+
+        [BenchmarkCategory("Delete"), Benchmark]
+        public void DeleteWithId()
+        {
+            _repo.Delete(_customer.Id);
+        }
+
+        [BenchmarkCategory("Delete"), Benchmark]
+        public void DeleteWithPredicate()
+        {
+            _repo.Delete(x => x.Id == _customer.Id);
+        }
+
+        [BenchmarkCategory("DeleteAsync"), Benchmark]
+        public async Task Async_Delete()
+        {
+            await _repo.DeleteAsync(_customer);
+        }
+
+        [BenchmarkCategory("DeleteAsync"), Benchmark]
+        public async Task Async_DeleteRange()
+        {
+            await _repo.DeleteAsync(new[] { _customer });
+        }
+
+        [BenchmarkCategory("DeleteAsync"), Benchmark]
+        public async Task Async_DeleteWithId()
+        {
+            await _repo.DeleteAsync(_customer.Id);
+        }
+
+        [BenchmarkCategory("DeleteAsync"), Benchmark]
+        public async Task Async_DeleteWithPredicate()
+        {
+            await _repo.DeleteAsync(x => x.Id == _customer.Id);
+        }
+    }
+}
