d0153836ccd66a9aef6720474d95f1aa50272a4c
Johelvis Guzman <johelvisguzman@gmail.com>
2019-02-26 16:59:11 -0500

    Added a unity container for the WPF demo application

---
+++ samples/DotNetToolkit.Repository.Wpf.Demo/Services/UnityServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetToolkit.Repository.Wpf.Demo.Services
+{
+    using Unity;
+
+    public class UnityServiceLocator : IServiceLocator
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityServiceLocator(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public T GetService<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
