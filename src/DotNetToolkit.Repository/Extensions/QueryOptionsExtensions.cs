94433c198d271f8dcfce30cbfab8aaf50c805556
Johelvis Guzman <johelvisguzman@gmail.com>
2019-03-20 16:13:56 -0400

    Allowed to initialized fetching strategy from a default instance

---
+++ src/DotNetToolkit.Repository/Extensions/QueryOptionsExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetToolkit.Repository.Extensions
+{
+    using Queries;
+    using Queries.Strategies;
+    using System.Linq;
+
+    /// <summary>
+    /// Contains various utility methods for applying options to the specified <see cref="IQueryOptions{T}" />.
+    /// </summary>
+    public static class QueryOptionsExtensions
+    {
+        /// <summary>
+        /// Returns the fetch strategy, or a default valued if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>The fetching strategy.</returns>
+        public static IFetchQueryStrategy<T> DefaultIfFetchStrategyEmpty<T>(this IQueryOptions<T> source)
+        {
+            return source?.FetchStrategy != null && source.FetchStrategy.PropertyPaths.Any()
+                ? source.FetchStrategy
+                : FetchQueryStrategy<T>.Default();
+        }
+    }
+}
