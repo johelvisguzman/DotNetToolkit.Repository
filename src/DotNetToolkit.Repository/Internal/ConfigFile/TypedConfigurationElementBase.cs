af7d2d0f9d0da5d7374e6a73f6aa6d95b58694ea
Johelvis Guzman <johelvisguzman@gmail.com>
2019-03-28 15:56:09 -0400

    Added changes to support keyvalues in the configuration files in addition to standard parameters

--- src/DotNetToolkit.Repository/Internal/ConfigFile/TypedConfigurationElementBase.cs
+++ src/DotNetToolkit.Repository/Internal/ConfigFile/TypedConfigurationElementBase.cs
@@ -2,13 +2,17 @@
 
 namespace DotNetToolkit.Repository.Internal.ConfigFile
 {
+    using Extensions;
     using System;
+    using System.Collections.Generic;
     using System.Configuration;
+    using System.Linq;
 
     internal abstract class TypedConfigurationElementBase<T> : ConfigurationElement
     {
         private const string TypeKey = "type";
         private const string ParametersKey = "parameters";
+        private const string KeyValuesKey = "keyValues";
 
         private Type _type;
 
@@ -43,8 +47,13 @@ namespace DotNetToolkit.Repository.Internal.ConfigFile
         [ConfigurationProperty(ParametersKey, IsRequired = false)]
         public ParameterCollection Parameters
         {
-            get => (ParameterCollection)this[ParametersKey];
-            set => this[ParametersKey] = value;
+            get { return (ParameterCollection)this[ParametersKey]; }
+        }
+
+        [ConfigurationProperty(KeyValuesKey, IsDefaultCollection = false)]
+        public KeyValueConfigurationCollection KeyValues
+        {
+            get { return (KeyValueConfigurationCollection)this[KeyValuesKey]; }
         }
 
         public virtual T GetTypedValue()
@@ -54,14 +63,48 @@ namespace DotNetToolkit.Repository.Internal.ConfigFile
             if (type == null)
                 return default(T);
 
-            var args = Parameters.GetTypedParameterValues();
-
             var defaultFactory = ConfigurationProvider.GetDefaultFactory();
 
             if (defaultFactory != null)
                 return (T)defaultFactory(type);
 
-            return (T)Activator.CreateInstance(type, args);
+            var args = GetArguments();
+
+            if (args != null && args.Any())
+                return (T)Activator.CreateInstance(type, args);
+
+            return (T)Activator.CreateInstance(type);
+        }
+
+        private object[] GetArguments()
+        {
+            var parameters = Parameters.GetTypedParameterValues();
+            if (parameters != null && parameters.Any())
+                return parameters;
+
+            var keyValues = KeyValues;
+            if (keyValues == null || keyValues.Count == 0)
+                return null;
+
+            var type = Type;
+            var keys = keyValues.AllKeys;
+            var matchedCtorParams = type
+                .GetConstructors()
+                .Select(x => x.GetParameters())
+                .FirstOrDefault(pi => pi
+                    .Select(x => x.Name)
+                    .OrderBy(x => x)
+                    .SequenceEqual(keys.OrderBy(x => x)));
+
+            if (matchedCtorParams == null)
+                return null;
+
+            var paramList = new List<object>();
+
+            paramList.AddRange(matchedCtorParams
+                .Select(ctorParam => ctorParam.ParameterType.ConvertTo(keyValues[ctorParam.Name].Value)));
+
+            return paramList.ToArray();
         }
     }
 }
