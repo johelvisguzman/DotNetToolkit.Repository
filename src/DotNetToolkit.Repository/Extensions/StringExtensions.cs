f0ecb68b634412edfff8557cb99a126fbf10ded2
Johelvis Guzman <johelvisguzman@gmail.com>
2019-05-14 21:26:16 -0400

    Moved the internal string hashing extensions to a Hasher utility class

--- src/DotNetToolkit.Repository/Extensions/StringExtensions.cs
+++ src/DotNetToolkit.Repository/Extensions/StringExtensions.cs
@@ -26,40 +26,6 @@
             return sb.ToString().Substring(1);
         }
 
-        public static string ToSHA256([NotNull] this string value)
-        {
-            Guard.NotEmpty(value, nameof(value));
-
-            var sha256 = System.Security.Cryptography.SHA256.Create();
-            var hashInBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(value));
-            var sb = new StringBuilder();
-
-            foreach (var b in hashInBytes)
-            {
-                sb.Append(b.ToString("x2"));
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ToMD5([NotNull] this string value)
-        {
-            Guard.NotEmpty(value, nameof(value));
-
-            var bytes = Encoding.Unicode.GetBytes(value.ToCharArray());
-
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                var hash = md5.ComputeHash(bytes);
-
-                return hash
-                    .Aggregate(
-                        new StringBuilder(32),
-                        (sb, b) => sb.Append(b.ToString("x2")))
-                    .ToString();
-            }
-        }
-
         public static bool ToBoolean(this string value)
         {
             if (value == null || value.Trim().Length == 0)
---
+++ src/DotNetToolkit.Repository/Utility/Hasher.cs
@@ -0,0 +1,36 @@
+﻿namespace DotNetToolkit.Repository.Utility
+{
+    using JetBrains.Annotations;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// Contains various utilities for hashing.
+    /// </summary>
+    internal static class Hasher
+    {
+        public static string ComputeMD5([NotNull] string value)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return ComputeHashAlgorithm(md5, value);
+            }
+        }
+
+        private static string ComputeHashAlgorithm([NotNull] HashAlgorithm algorithm, [NotNull] string value)
+        {
+            Guard.NotNull(algorithm, nameof(algorithm));
+            Guard.NotEmpty(value, nameof(value));
+
+            var bytes = Encoding.Unicode.GetBytes(value.ToCharArray());
+            var hash = algorithm.ComputeHash(bytes);
+
+            return hash
+                .Aggregate(
+                    new StringBuilder(32),
+                    (sb, b) => sb.Append(b.ToString("x2")))
+                .ToString();
+        }
+    }
+}
