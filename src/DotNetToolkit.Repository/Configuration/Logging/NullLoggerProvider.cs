1ef532c43636996b08fd429c9335d1c7955ba084
jaguzman <jguzman@crahealth.com>
2019-07-16 12:50:14 -0400

    Changed the internal NullLoggerProvider to be public

---
+++ src/DotNetToolkit.Repository/Configuration/Logging/NullLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace DotNetToolkit.Repository.Configuration.Logging
+{
+    /// <summary>
+    /// An implementation of <see cref="ILoggerProvider" />.
+    /// </summary>
+    public class NullLoggerProvider : ILoggerProvider
+    {
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        public static NullLoggerProvider Instance { get; } = new NullLoggerProvider();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NullLoggerProvider"/> class.
+        /// </summary>
+        private NullLoggerProvider() { }
+
+        /// <summary>
+        /// Creates a new <see cref="ILogger"/> instance.
+        /// </summary>
+        /// <param name="categoryName">The category name for messages produced by the logger.</param>
+        /// <returns>The <see cref="ILogger"/>.</returns>
+        public ILogger Create(string categoryName)
+        {
+            return Internal.NullLogger.Instance;
+        }
+    }
+}
