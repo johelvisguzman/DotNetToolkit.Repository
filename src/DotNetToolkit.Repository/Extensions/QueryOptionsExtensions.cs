4630267bfd055337a03273ceb1202b718f69262e
Johelvis Guzman <johelvisguzman@gmail.com>
2021-09-18 16:51:35 -0400

    (GH-638) Add new context base for handling enumerable linq operations

--- src/DotNetToolkit.Repository/Extensions/QueryOptionsExtensions.cs
+++ src/DotNetToolkit.Repository/Extensions/QueryOptionsExtensions.cs
@@ -1,11 +1,8 @@
 ﻿namespace DotNetToolkit.Repository.Extensions
 {
-    using Configuration.Conventions;
     using JetBrains.Annotations;
     using Query;
-    using Query.Strategies;
     using System;
-    using System.Linq;
     using System.Linq.Expressions;
     using Utility;
 
@@ -14,22 +11,6 @@
     /// </summary>
     public static class QueryOptionsExtensions
     {
-        /// <summary>
-        /// Returns the fetch strategy, or a default valued if the sequence is empty.
-        /// </summary>
-        /// <typeparam name="T">The type of the entity.</typeparam>
-        /// <param name="source">The source.</param>
-        /// <param name="conventions">The configurable conventions.</param>
-        /// <returns>The fetching strategy.</returns>
-        public static IFetchQueryStrategy<T> DefaultIfFetchStrategyEmpty<T>([CanBeNull] this IQueryOptions<T> source, [NotNull] IRepositoryConventions conventions)
-        {
-            Guard.NotNull(conventions, nameof(conventions));
-
-            return source?.FetchStrategy != null && source.FetchStrategy.PropertyPaths.Any()
-                ? source.FetchStrategy
-                : FetchQueryStrategy<T>.Default(conventions);
-        }
-
         /// <summary>
         /// Converts the specified predicate to <see cref="IQueryOptions{T}" />.
         /// </summary>
