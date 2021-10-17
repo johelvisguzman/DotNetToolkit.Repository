13127094f500f005ea3653ef35a726fde55dfff5
Johelvis Guzman <johelvisguzman@gmail.com>
2021-10-17 14:18:44 -0400

    (GH-613) Re-factor memcached caching provider

--- src/DotNetToolkit.Repository.Caching.Memcached/MemcachedCacheProvider.cs
+++ src/DotNetToolkit.Repository.Caching.Memcached/MemcachedCacheProvider.cs
@@ -5,139 +5,27 @@
     using Enyim.Caching.Configuration;
     using Enyim.Caching.Memcached;
     using JetBrains.Annotations;
+    using Newtonsoft.Json;
     using System;
     using Utility;
 
-    /// <summary>
-    /// An implementation of <see cref="ICacheProvider{TCache}" />.
-    /// </summary>
-    public class MemcachedCacheProvider : CacheProviderBase<MemcachedCache>
+    internal class MemcachedCacheProvider : ICacheProvider
     {
+        #region Properties
+
+        public IMemcachedClient Client { get; }
+        public TimeSpan? Expiry { get; set; }
+
+        #endregion
+
         #region Constructors
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the default configuration section (enyim/memcached). 
-        /// </summary>
-        public MemcachedCacheProvider() : this((TimeSpan?)null) { }
+        public MemcachedCacheProvider(string host, string username, string password, MemcachedProtocol protocol, Type authType, TimeSpan? expiry)
+            : this(GetClientConfiguration(host, username, password, protocol, authType), expiry) { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the default configuration section (enyim/memcached). 
-        /// </summary>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([CanBeNull] TimeSpan? expiry) : this(new MemcachedClient(), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified configuration section.
-        /// </summary>
-        /// <param name="sectionName">The name of the configuration section to be used for configuring the behavior of the client.</param>
-        public MemcachedCacheProvider([NotNull] string sectionName) : this(sectionName, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified configuration section.
-        /// </summary>
-        /// <param name="sectionName">The name of the configuration section to be used for configuring the behavior of the client.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] string sectionName, [CanBeNull] TimeSpan? expiry) : this(new MemcachedClient(Guard.NotEmpty(sectionName, nameof(sectionName))), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port) : this(host, port, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, [CanBeNull] TimeSpan? expiry) : this(host, port, MemcachedProtocol.Binary, expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="protocol">The type of the communication between client and server.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, MemcachedProtocol protocol) : this(host, port, protocol, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="protocol">The type of the communication between client and server.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, MemcachedProtocol protocol, [CanBeNull] TimeSpan? expiry) : this(GetConfiguredClient(host, port, protocol), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, [NotNull] string username, [NotNull] string password) : this(host, port, username, password, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, [NotNull] string username, [NotNull] string password, [CanBeNull] TimeSpan? expiry) : this(host, port, username, password, MemcachedProtocol.Binary, expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        /// <param name="protocol">The type of the communication between client and server.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, [NotNull] string username, [NotNull] string password, MemcachedProtocol protocol) : this(host, port, username, password, protocol, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="port">The port.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        /// <param name="protocol">The type of the communication between client and server.</param>
-        public MemcachedCacheProvider([NotNull] string host, int port, [NotNull] string username, [NotNull] string password, MemcachedProtocol protocol, [CanBeNull] TimeSpan? expiry) : this(GetConfiguredClient(host, port, username, password, protocol), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified client.
-        /// </summary>
-        /// <param name="configActions">The configuration actions.</param>
-        public MemcachedCacheProvider([NotNull] Action<MemcachedClientConfiguration> configActions) : this(configActions, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified client.
-        /// </summary>
-        /// <param name="configActions">The configuration actions.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] Action<MemcachedClientConfiguration> configActions, [CanBeNull] TimeSpan? expiry) : this(GetConfiguredClient(configActions), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified client.
-        /// </summary>
-        /// <param name="client">The memcached client.</param>
-        public MemcachedCacheProvider([NotNull] IMemcachedClient client) : this(client, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MemcachedCacheProvider" /> class using the specified client.
-        /// </summary>
-        /// <param name="client">The memcached client.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public MemcachedCacheProvider([NotNull] IMemcachedClient client, [CanBeNull] TimeSpan? expiry)
+        private MemcachedCacheProvider(IMemcachedClientConfiguration config, TimeSpan? expiry)
         {
-            Cache = new MemcachedCache(Guard.NotNull(client, nameof(client)));
+            Client = new MemcachedClient(Guard.NotNull(config, nameof(config)));
             Expiry = expiry;
         }
 
@@ -145,44 +33,105 @@
 
         #region Private Methods
 
-        private static IMemcachedClient GetConfiguredClient(Action<MemcachedClientConfiguration> configActions)
+        private static string Serialize(object o)
         {
-            Guard.NotNull(configActions, nameof(configActions));
+            if (o == null)
+                return null;
 
-            var config = new MemcachedClientConfiguration();
-
-            configActions(config);
-
-            return new MemcachedClient(config);
+            return JsonConvert.SerializeObject(o);
         }
 
-        private static IMemcachedClient GetConfiguredClient(string host, int port, MemcachedProtocol protocol)
+        private static T Deserialize<T>(string v)
         {
-            Guard.NotEmpty(host, nameof(host));
+            if (string.IsNullOrEmpty(v))
+                return default(T);
 
-            var config = new MemcachedClientConfiguration();
-
-            config.AddServer(host, port);
-            config.Protocol = protocol;
-
-            return new MemcachedClient(config);
+            return JsonConvert.DeserializeObject<T>(v);
         }
 
-        private static IMemcachedClient GetConfiguredClient(string host, int port, string username, string password, MemcachedProtocol protocol)
+        private static IMemcachedClientConfiguration GetClientConfiguration(string host, string username, string password, MemcachedProtocol protocol, Type authType = null)
         {
-            Guard.NotEmpty(host, nameof(host));
-            Guard.NotEmpty(username, nameof(username));
-            Guard.NotEmpty(password, nameof(password));
-
             var config = new MemcachedClientConfiguration();
 
-            config.AddServer(host, port);
-            config.Protocol = protocol;
-            config.Authentication.Type = typeof(PlainTextAuthenticator);
-            config.Authentication.Parameters["userName"] = username;
-            config.Authentication.Parameters["password"] = password;
+            if (!string.IsNullOrEmpty(host))
+            {
+                config.AddServer(host);
+            }
 
-            return new MemcachedClient(config);
+            config.Protocol = protocol;
+            config.Authentication.Type = authType;
+            
+            if (!string.IsNullOrEmpty(username))
+            {
+                config.Authentication.Parameters["userName"] = username;
+            }
+
+            if (!string.IsNullOrEmpty(password))
+            {
+                config.Authentication.Parameters["password"] = password;
+            }
+
+            return config;
+        }
+
+        private void Set<T>(string key, T value, TimeSpan? expiry)
+        {
+            if (expiry.HasValue)
+            {
+                Client.Store(StoreMode.Set, key, Serialize(value), expiry.Value);
+            }
+            else
+            {
+                Client.Store(StoreMode.Set, key, Serialize(value));
+            }
+        }
+
+        #endregion
+
+        #region Implementation of ICacheProvider
+
+        public void Set<T>(string key, T value, TimeSpan? expiry = null, Action<string> cacheRemovedCallback = null)
+        {
+            Guard.NotEmpty(key, nameof(key));
+
+            Set(key, value, expiry ?? Expiry);
+        }
+
+        public void Remove([NotNull] string key)
+        {
+            Client.Remove(Guard.NotEmpty(key, nameof(key)));
+        }
+
+        public bool TryGetValue<T>(string key, out T value)
+        {
+            try
+            {
+                value = Deserialize<T>(Client.Get<string>(Guard.NotEmpty(key, nameof(key))));
+
+                if (Equals(value, default(T)))
+                {
+                    value = default(T);
+
+                    return false;
+                }
+            }
+            catch
+            {
+                value = default(T);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public int Increment(string key, int defaultValue, int incrementValue)
+        {
+            Guard.NotEmpty(key, nameof(key));
+
+            var value = Client.Increment(key, Convert.ToUInt64(defaultValue), Convert.ToUInt64(incrementValue));
+
+            return Convert.ToInt32(value);
         }
 
         #endregion
