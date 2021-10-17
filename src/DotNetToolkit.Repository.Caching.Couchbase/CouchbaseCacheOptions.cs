27949d77626588c8095987aa47af6823e287f54a
Johelvis Guzman <johelvisguzman@gmail.com>
2021-10-17 14:19:31 -0400

    (GH-613) Re-factor couchbase caching provider

---
+++ src/DotNetToolkit.Repository.Caching.Couchbase/CouchbaseCacheOptions.cs
@@ -0,0 +1,112 @@
+﻿namespace DotNetToolkit.Repository.Caching.Couchbase
+{
+    using JetBrains.Annotations;
+    using System;
+    using Utility;
+
+    /// <summary>
+    /// The options to be used by the couchbase caching provider.
+    /// </summary>
+    public class CouchbaseCacheOptions
+    {
+        private string _host;
+        private string _bucketName;
+        private string _username;
+        private string _password;
+        private TimeSpan? _expiry;
+
+        /// <summary>
+        /// Gets the host.
+        /// </summary>
+        public string Host { get { return _host; } }
+
+        /// <summary>
+        /// Gets the bucket name.
+        /// </summary>
+        public string BucketName { get { return _bucketName; } }
+
+        /// <summary>
+        /// Gets the username.
+        /// </summary>
+        public string Username { get { return _username; } }
+
+        /// <summary>
+        /// Gets the password.
+        /// </summary>
+        public string Password { get { return _password; } }
+
+        /// <summary>
+        /// Gets the expiration time.
+        /// </summary>
+        public TimeSpan? Expiry { get { return _expiry; } }
+
+        /// <summary>
+        /// Adds the giving bucketname to the options.
+        /// </summary>
+        /// <param name="bucketname">The bucketname to be added.</param>
+        public CouchbaseCacheOptions WithBucketName([NotNull] string bucketname)
+        {
+            _bucketName = Guard.NotEmpty(bucketname, nameof(bucketname));
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the giving username to the options.
+        /// </summary>
+        /// <param name="username">The username to be added.</param>
+        public CouchbaseCacheOptions WithUsername([NotNull] string username)
+        {
+            _username = Guard.NotEmpty(username, nameof(username));
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the giving password to the options.
+        /// </summary>
+        /// <param name="password">The password to be added.</param>
+        public CouchbaseCacheOptions WithPassword([NotNull] string password)
+        {
+            _password = Guard.NotEmpty(password, nameof(password));
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the giving endpoint to the options.
+        /// </summary>
+        /// <param name="host">The host to be added.</param>
+        /// <param name="port">The port to be added.</param>
+        public CouchbaseCacheOptions WithEndPoint([NotNull] string host, int port)
+        {
+            Guard.NotEmpty(host, nameof(host));
+
+            _host = string.Format("{0}:{1}", host, port);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the giving endpoint to the options.
+        /// </summary>
+        /// <param name="hostAndPort">The host and port to be added.</param>
+        public CouchbaseCacheOptions WithEndPoint([NotNull] string hostAndPort)
+        {
+            _host = Guard.NotEmpty(hostAndPort, nameof(hostAndPort));
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the giving caching expiration time to the options.
+        /// </summary>
+        /// <param name="expiry">The caching expiration time to be added.</param>
+        public CouchbaseCacheOptions WithExpiry([NotNull] TimeSpan expiry)
+        {
+            _expiry = Guard.NotNull(expiry, nameof(expiry));
+
+            return this;
+        }
+    }
+}
--- src/DotNetToolkit.Repository.Caching.Couchbase/CouchbaseCacheProvider.cs
+++ src/DotNetToolkit.Repository.Caching.Couchbase/CouchbaseCacheProvider.cs
@@ -1,114 +1,35 @@
 ﻿namespace DotNetToolkit.Repository.Caching.Couchbase
 {
     using Configuration.Caching;
+    using global::Couchbase;
     using global::Couchbase.Configuration.Client;
-    using JetBrains.Annotations;
     using System;
     using System.Collections.Generic;
     using Utility;
 
-    /// <summary>
-    /// An implementation of <see cref="ICacheProvider{TCache}" />.
-    /// </summary>
-    public class CouchbaseCacheProvider : CacheProviderBase<CouchbaseCache>
+    internal class CouchbaseCacheProvider : ICacheProvider
     {
+        #region Fields
+
+        private const string DefaultBucketName = "default";
+
+        #endregion
+
+        #region Properties
+
+        public Cluster Cluster { get; }
+        public TimeSpan? Expiry { get; set; }
+
+        #endregion
+
         #region Constructors
 
-#if NETFULL
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class. 
-        /// </summary>
-        /// <param name="sectionName">The name of the configuration section to be used for configuring the behavior of the couchbase client.</param>
-        public CouchbaseCacheProvider([NotNull] string sectionName) : this(sectionName, (TimeSpan?)null) { }
+        public CouchbaseCacheProvider(string host, string bucketName, string username, string password, TimeSpan? expiry)
+            : this(GetConfigurationOptions(host, bucketName, username, password), expiry) { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class. 
-        /// </summary>
-        /// <param name="sectionName">The name of the configuration section to be used for configuring the behavior of the couchbase client.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public CouchbaseCacheProvider([NotNull] string sectionName, [CanBeNull] TimeSpan? expiry)
+        private CouchbaseCacheProvider(ClientConfiguration config, TimeSpan? expiry)
         {
-            Cache = new CouchbaseCache(Guard.NotEmpty(sectionName, nameof(sectionName)));
-            Expiry = expiry;
-        } 
-#endif
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class. 
-        /// </summary>
-        /// <remarks>This will connect to a single server on the local machine.</remarks>
-        public CouchbaseCacheProvider() : this((TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class. 
-        /// </summary>
-        /// <param name="expiry">The caching expiration time.</param>
-        /// <remarks>This will connect to a single server on the local machine.</remarks>
-        public CouchbaseCacheProvider([CanBeNull] TimeSpan? expiry) : this(new ClientConfiguration(), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        public CouchbaseCacheProvider([NotNull] string host, [NotNull] string username, [NotNull] string password) : this(host, username, password, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public CouchbaseCacheProvider([NotNull] string host, [NotNull] string username, [NotNull] string password, [CanBeNull] TimeSpan? expiry) : this(host, null, username, password, expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="bucketName">The bucket name.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        public CouchbaseCacheProvider([NotNull] string host, [NotNull] string bucketName, [NotNull] string username, [NotNull] string password) : this(host, bucketName, username, password, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="host">The host name.</param>
-        /// <param name="bucketName">The bucket name.</param>
-        /// <param name="username">The username to use to authenticate with the server.</param>
-        /// <param name="password">The password to use to authenticate with the server.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public CouchbaseCacheProvider([NotNull] string host, string bucketName, [NotNull] string username, [NotNull] string password, [CanBeNull] TimeSpan? expiry) : this(GetConfigurationOptions(host, username, password, bucketName), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="configAction">The configuration options action.</param>
-        public CouchbaseCacheProvider([NotNull] Action<ClientConfiguration> configAction) : this(configAction, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="configAction">The configuration options action.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public CouchbaseCacheProvider([NotNull] Action<ClientConfiguration> configAction, [CanBeNull] TimeSpan? expiry) : this(GetConfigurationOptions(configAction), expiry) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="config">The configuration options to use for the couchbase client.</param>
-        public CouchbaseCacheProvider([NotNull] ClientConfiguration config) : this(config, (TimeSpan?)null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CouchbaseCacheProvider" /> class.
-        /// </summary>
-        /// <param name="config">The configuration options to use for the couchbase client.</param>
-        /// <param name="expiry">The caching expiration time.</param>
-        public CouchbaseCacheProvider([NotNull] ClientConfiguration config, [CanBeNull] TimeSpan? expiry)
-        {
-            Cache = new CouchbaseCache(Guard.NotNull(config, nameof(config)));
+            Cluster = new Cluster(Guard.NotNull(config, nameof(config)));
             Expiry = expiry;
         }
 
@@ -116,40 +37,91 @@
 
         #region Private Methods
 
-        private static ClientConfiguration GetConfigurationOptions(Action<ClientConfiguration> configAction)
+        private static ClientConfiguration GetConfigurationOptions(string host, string username, string password, string bucketName)
         {
-            Guard.NotNull(configAction, nameof(configAction));
-
             var config = new ClientConfiguration();
+            var bucket = !string.IsNullOrEmpty(bucketName) ? bucketName : DefaultBucketName;
 
-            configAction(config);
+            if (!string.IsNullOrEmpty(host))
+            {
+                config.Servers = new List<Uri>
+                {
+                    { new Uri(host) }
+                };
+            }
+
+            config.BucketConfigs = new Dictionary<string, BucketConfiguration>
+            {
+                { bucket, new BucketConfiguration
+                    {
+                        Username = username,
+                        Password = password,
+                        BucketName = bucket
+                    }
+                }
+            };
 
             return config;
         }
 
-        private static ClientConfiguration GetConfigurationOptions(string host, string username, string password, string bucketName)
+        private void Set<T>(string key, T value, TimeSpan? expiry)
         {
-            Guard.NotEmpty(host, nameof(host));
-            Guard.NotEmpty(username, nameof(username));
-            Guard.NotEmpty(password, nameof(password));
-
-            if (string.IsNullOrEmpty(bucketName))
-                bucketName = "default";
-
-            return new ClientConfiguration
+            using (var bucket = Cluster.OpenBucket())
             {
-                Servers = new List<Uri> { new Uri(host) },
-                BucketConfigs = new Dictionary<string, BucketConfiguration>
+                if (expiry.HasValue)
                 {
-                    { bucketName, new BucketConfiguration
-                        {
-                            Password = password,
-                            Username = username,
-                            BucketName = bucketName
-                        }
-                    }
+                    bucket.Insert(key, value, expiry.Value);
                 }
-            };
+                else
+                {
+                    bucket.Insert(key, value);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Implementation of ICacheProvider
+
+        public void Set<T>(string key, T value, TimeSpan? expiry, Action<string> cacheRemovedCallback = null)
+        {
+            Guard.NotEmpty(key, nameof(key));
+
+            Set(key, value, expiry ?? Expiry);
+        }
+
+        public void Remove(string key)
+        {
+            using (var bucket = Cluster.OpenBucket())
+            {
+                bucket.Remove(Guard.NotEmpty(key, nameof(key)));
+            }
+        }
+
+        public bool TryGetValue<T>(string key, out T value)
+        {
+            Guard.NotEmpty(key, nameof(key));
+
+            using (var bucket = Cluster.OpenBucket())
+            {
+                var operationResult = bucket.Get<T>(key);
+
+                value = operationResult.Value;
+
+                return operationResult.Success;
+            }
+        }
+
+        public int Increment(string key, int defaultValue, int incrementValue)
+        {
+            Guard.NotEmpty(key, nameof(key));
+
+            using (var bucket = Cluster.OpenBucket())
+            {
+                var operationResult = bucket.Increment(key, Convert.ToUInt64(defaultValue), Convert.ToUInt64(incrementValue));
+
+                return Convert.ToInt32(operationResult.Value);
+            }
         }
 
         #endregion
