97277cbe3ba556df959ca1fb738c598d624acd70
Johelvis Guzman <johelvisguzman@gmail.com>
2021-10-17 14:19:43 -0400

    (GH-613) Add new options buider extensions to the couchbase caching provider

---
+++ src/DotNetToolkit.Repository.Caching.Couchbase/RepositoryOptionsBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace DotNetToolkit.Repository.Caching.Couchbase
+{
+    using Configuration.Options;
+    using JetBrains.Annotations;
+    using System;
+    using Utility;
+
+    /// <summary>
+    /// Contains various extension methods for <see cref="RepositoryOptionsBuilder" />
+    /// </summary>
+    public static class RepositoryOptionsBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the caching provider to use couchbase.
+        /// </summary>
+        /// <param name="source">The repository options builder.</param>
+        /// <param name="optionsAction">The options action.</param>
+        /// <returns>The same builder instance.</returns>
+        public static RepositoryOptionsBuilder UseCouchbase([NotNull] this RepositoryOptionsBuilder source, [NotNull] Action<CouchbaseCacheOptions> optionsAction)
+        {
+            Guard.NotNull(source, nameof(source));
+            Guard.NotNull(optionsAction, nameof(optionsAction));
+
+            var options = new CouchbaseCacheOptions();
+
+            optionsAction(options);
+
+            var provider = new CouchbaseCacheProvider(
+                options.Host,
+                options.BucketName,
+                options.Username,
+                options.Password,
+                options.Expiry);
+
+            source.UseCachingProvider(provider);
+
+            return source;
+        }
+    }
+}
