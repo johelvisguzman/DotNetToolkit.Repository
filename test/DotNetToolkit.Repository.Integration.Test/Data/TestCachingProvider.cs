31fe39eabfc47b9641dc6aa52d6e56181d8807d6
Johelvis Guzman <johelvisguzman@gmail.com>
2022-01-10 14:18:19 -0500

    (GH-677) Add unit tests

---
+++ test/DotNetToolkit.Repository.Integration.Test/Data/TestCachingProvider.cs
@@ -0,0 +1,38 @@
+﻿namespace DotNetToolkit.Repository.Integration.Test.Data
+{
+    using Configuration.Caching;
+    using System;
+    using System.Collections.Concurrent;
+
+    public class TestCachingProvider : ICacheProvider
+    {
+        private static readonly ConcurrentDictionary<string, object> _cache = new ConcurrentDictionary<string, object>();
+
+        public int Increment(string key, int defaultValue, int incrementValue)
+        {
+            return (int)_cache.AddOrUpdate(key, defaultValue, (k, oldValue) => (int)oldValue + 1);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.TryRemove(key, out _);
+        }
+
+        public void Set<T>(string key, T value, TimeSpan? expiry = null, Action<string> cacheRemovedCallback = null)
+        {
+            _cache.AddOrUpdate(key, value, (k, oldValue) => value);
+        }
+
+        public bool TryGetValue<T>(string key, out T value)
+        {
+            if (_cache.TryGetValue(key, out object obj))
+            {
+                value = (T)obj;
+                return true;
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
